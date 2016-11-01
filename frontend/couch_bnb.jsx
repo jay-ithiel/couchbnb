a14641255b4e6c3ec803111cadd14ad9e7d3cd4a
Jay Hwang <jayhwang0121@gmail.com>
2016-10-31 22:44:38 -0700

    add app.jsx file

---
+++ frontend/couch_bnb.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const mainRoot = document.getElementById("root");
+  ReactDOM.render(<h2>This is react Root</h2>, mainRoot);
+});
