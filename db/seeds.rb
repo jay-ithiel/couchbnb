6b5cdb8eaf4001c0071b482a02cf367e54220577
Jay Hwang <jayhwang0121@gmail.com>
2016-11-06 17:14:04 -0800

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,3 +36,20 @@ Spot.create({
   max_guests: 4,
   spot_pic_url: 'https://www.google.com',
 })
+
+Spot.create({
+  host_id: 2,
+  host_name: "Host",
+  title: "Los Angeles Apt Downtown",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  country: "United States",
+  state_region: "California",
+  city: "Los Angeles",
+  post_code: '91002',
+  street_address: '12345 Wilshire Blvd',
+  price_per_night: '$100',
+  room_type: "Apartment",
+  bed_count: 4,
+  max_guests: 4,
+  spot_pic_url: 'https://www.google.com',
+})
