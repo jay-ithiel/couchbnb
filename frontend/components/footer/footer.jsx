44c283e4b372b58094acdef2773b142be0c22dd0
Jay Hwang <jayhwang0121@gmail.com>
2017-09-27 12:42:45 -0700

    Changed footer links"O

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,14 +3,21 @@ import React from 'react';
 const Footer = () => (
   <div className="footer">
     <ul className="links-container">
-      <li className="footer-link">ABOUT</li>
-      <li className="footer-link">GETTING STARTED</li>
-      <li className="footer-link">SITEMAP</li>
-      <li className="footer-link">SAFETY</li>
-      <li className="footer-link">COMMUNITY</li>
-      <li className="footer-link">SUPPORT</li>
-      <li className="footer-link">TERMS</li>
-      <li className="footer-link">PRIVACY</li>
+      <li>
+        <a className="footer-link" target="_blank" href="http://www.jayhwang.xyz">ABOUT</a>
+      </li>
+
+      <li>
+        <a className="footer-link" target="_blank" href="https://github.com/jay-hwang/couchbnb">SOURCE</a>
+      </li>
+
+      <li>
+        <a className="footer-link" target="_blank" href="https://github.com/jay-hwang/couchbnb/issues">SUPPORT</a>
+      </li>
+
+      <li>
+        <a className="footer-link" target="_blank" href="https://www.linkedin.com/in/jay-hwang">COMPANY</a>
+      </li>
     </ul>
   </div>
 );
